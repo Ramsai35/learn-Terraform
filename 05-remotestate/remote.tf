79aabb01dad1620f4f5491fc7407f682d73b8532
Ramsai35 <mudurukollaramsai335@gmail.com>
2023-02-09 12:10:53 +0530

    create instance

---
+++ 05-remotestate/remote.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ami" "centos8" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+resource "aws_instance" "test" {
+  ami   = data.aws_ami.centos8.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Test-Centos"
+  }
+
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-ramsai"
+    key    = "05-remotestate/terraform.tfstate"
+    region = "us-east-1"
+  }
+}

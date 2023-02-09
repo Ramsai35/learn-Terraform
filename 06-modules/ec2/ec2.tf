e4f62c87076a1ba1c824f56636f94c37c0b0c723
Ramsai35 <mudurukollaramsai335@gmail.com>
2023-02-09 17:31:16 +0530

    tranfering data one module to another module

--- 06-modules/ec2/ec2.tf
+++ 06-modules/ec2/ec2.tf
@@ -1,8 +1,10 @@
 resource "aws_instance" "test" {
-  ami   = data.aws_ami.centos8.id
-  instance_type = "t3.micro"
+  ami                    = data.aws_ami.centos8.id
+  instance_type          = "t3.micro"
   vpc_security_group_ids = [var.security_group_Id]
 
   tags = {
     Name = "Test-Centos"
   }
+
+}

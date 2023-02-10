15c22d6693984f811a2e75ae90d00b26a9c1ec84
Ramsai35 <mudurukollaramsai335@gmail.com>
2023-02-11 03:18:50 +0530

    loop

--- 08-loops/main.tf
+++ 08-loops/main.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "web" {
-  count = 2
+  count = length(var.components)
   ami   = data.aws_ami.centos8.id
   instance_type = "t3.micro"
 
 
   tags = {
-    Name = "test-centos"
+    Name = var.components[count.index]
   }
 
 }
@@ -18,4 +18,8 @@ data "aws_ami" "centos8" {
 
 output "public_ip" {
   value = aws_instance.web.*.public_ip
+}
+
+variable "components" {
+  default = ["cart","catalogue"]
 }

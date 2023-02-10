f8ac3c7a73f77ab395d2109ec52ed0c07be18cff
Ramsai35 <mudurukollaramsai335@gmail.com>
2023-02-11 04:20:06 +0530

    loop

--- 08-loops/main.tf
+++ 08-loops/main.tf
@@ -16,9 +16,14 @@ data "aws_ami" "centos8" {
   owners      = ["973714476881"]
 }
 
-#output "public_ip" {
+output "publicip" {
 #  value = aws_instance.web.*.public_ip
 #}
+  value = {
+    for k,v in aws_instance.web : k => v.public_ip
+  }
+  }
+
 
 variable "components" {
   default = {
